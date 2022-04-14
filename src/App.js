2f337a6d8635ae39467df82b553a4a80ba5c416e
javohirToshmurodov <rustamnuraliyev1@mail.ru>
2022-04-14 11:21:09 +0500

    almost finished

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Routes, Route } from "react-router-dom";
 import { fetchForecast } from "./redux/global";
 import { WeatherDateList } from "./components/WeatherDateList";
 import WeatherHourly from "./components/WeatherHourly";
-import BgEffect from "./components/BgEffect";
 const App = () => {
   const dispatch = useDispatch();
 
@@ -14,12 +13,11 @@ const App = () => {
   }, [dispatch]);
   return (
     <Container>
-      <BgEffect />
       <Routes>
         <Route path="/" element={<WeatherDateList />} />
         <Route path="/:date" element={<WeatherHourly />} />
       </Routes>
-    </Container>  
+    </Container>
   );
 };
 
